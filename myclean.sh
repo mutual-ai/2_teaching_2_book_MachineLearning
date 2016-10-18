edc1b8bce16e6e8b79a7c8661dae038dd45494f7
Salman Aslam <salman@gatech.edu>
2016-10-18 18:32:25 +0500

    adding a cleanup file

---
+++ myclean.sh
@@ -0,0 +1,15 @@
+rm *.out 
+rm *.aux
+rm *.synctex.gz
+rm *.log
+rm *.toc
+rm *.nav
+rm *.snm
+rm *.blg
+rm *.bbl
+rm *.run.xml
+
+rm Water-blx.bib
+rm "test.synctex.gz(busy)"
+rm "test2.synctex.gz(busy)"
+rm "Water.synctex.gz(busy)"
